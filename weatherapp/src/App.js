d21f3af77bdee4b2258a57ebfd614711b8512262
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-29 11:10:01 -0400

    Creating new react app

---
+++ weatherapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DailyTemp from './components/DailyTemp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DailyTemp />
+    </div>
+  );
+}
+
+export default App;
