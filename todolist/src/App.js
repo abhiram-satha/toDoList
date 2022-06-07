7e70fa2987a23b085ccec6bf86997ddb7f32bfbf
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-06 23:55:00 -0400

    Setting up useEffect and useState

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import AddToDo from './components/AddToDo';
 import ToDoList from './components/ToDoList';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+
+  const [toDos, setToDos] = useState([])
+
+  useEffect(()=> {
+
+  }, [])
+
   return (
     <div className="App">
       <AddToDo />
